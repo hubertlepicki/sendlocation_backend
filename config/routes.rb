8a3f3d73ca09ae5aa33efa0f2e6e11c96069336f
Hubert <hubert.lepicki@amberbit.com>
2011-01-21 20:36:23 +0100

    displaying map by params

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 SendlocationBackend::Application.routes.draw do
-  match "map/:lat/:lng/:zoom_level/:pointer_type/:description", to: "maps#show", as: :map
+  match "map/:lat/:lng/:zoom_level/:marker_type/:description", to: "maps#show", as: :map,
+    :constraints => { :lat => /.*/, :lng => /.*/, :description => /.*/}
 end
---
+++ public/javascripts/application.js
@@ -0,0 +1,28 @@
+$(function() {
+  if ($("#map_canvas").size() == 1) {
+    var latlng = new google.maps.LatLng(
+      parseFloat($("#map_canvas").attr('data-lat')),
+      parseFloat($("#map_canvas").attr('data-lng'))
+    );
+    var myOptions = {
+      zoom: parseInt($("#map_canvas").attr('data-zoom')),
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    var marker = new google.maps.Marker({
+                   position: latlng, 
+                   map: map,
+                   title: $("#map_canvas").attr('data-description')
+                 }); 
+    var infowindow = new google.maps.InfoWindow({
+          content: $("#map_canvas").attr('data-description')
+    });
+    infowindow.open(map,marker);
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+  }
+});
