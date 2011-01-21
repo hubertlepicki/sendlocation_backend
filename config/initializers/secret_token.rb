c6f0ff79daeb2f4fdb82c9c5a68e14d29143a8d0
Hubert <hubert.lepicki@amberbit.com>
2011-01-21 19:27:19 +0100

    rails 3 app scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendlocationBackend::Application.config.secret_token = '7137a3f5e43ac03098e029e2cdf429ee81c3b6c7c19460ac515ff6933a8a04763fd1edd9e9134290b4cd32759a6e58031bc63c0d18e3fe93731c8b5c63337a68'
