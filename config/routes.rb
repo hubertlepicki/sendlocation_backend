02ae84f3f2671a977aa7d0f1a9dece2bf0bf39c7
Hubert <hubert.lepicki@amberbit.com>
2011-01-22 16:45:54 +0100

    allow empty description

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SendlocationBackend::Application.routes.draw do
   match "map/:lat/:lng/:zoom_level/:marker_type/:description", to: "maps#show", as: :map,
     :constraints => { :lat => /.*/, :lng => /.*/, :description => /.*/}
+  match "map/:lat/:lng/:zoom_level/:marker_type", to: "maps#show", as: :map,
+    :constraints => { :lat => /.*/, :lng => /.*/}
+
 end
